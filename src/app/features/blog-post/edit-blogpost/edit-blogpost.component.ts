e65f564f3b940b209f3c545b146041a53310ef2e
Izet Delibasic <izetdelibasic55@gmail.com>
2024-08-17 17:56:53 +0200

    Fetching and displaying data in edit component

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { BlogPostService } from '../services/blog-post.service';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -9,17 +11,32 @@ import { Subscription } from 'rxjs';
 })
 export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
+  model?: BlogPost;
   routeSubscription?: Subscription;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private blogPostService: BlogPostService
+  ) {}
 
   ngOnInit(): void {
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        // Get BlogPost from API
+        if (this.id) {
+          this.blogPostService.getBlogPostById(this.id).subscribe({
+            next: (response) => {
+              this.model = response;
+            },
+          });
+        }
       },
     });
   }
+
+  onFormSubmit(): void {}
 
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -21,4 +21,10 @@ export class BlogPostService {
   getAllBlogPosts(): Observable<BlogPost[]> {
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/BlogPost`);
   }
+
+  getBlogPostById(id: string): Observable<BlogPost> {
+    return this.http.get<BlogPost>(
+      `${environment.apiBaseUrl}/api/BlogPost/${id}`
+    );
+  }
 }
