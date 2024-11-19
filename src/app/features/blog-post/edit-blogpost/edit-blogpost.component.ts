b42f9ca8d07cd54193089fbb4eb23c9327c759f9
Izet Delibasic <izetdelibasic55@gmail.com>
2024-11-19 21:59:28 +0100

    Creating functionality to updating blogposts on client side

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
 import { CategoryService } from '../../category/services/category.service';
 import { Category } from '../../category/models/category.model';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -17,11 +18,13 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   categories$?: Observable<Category[]>;
   selectedCategories?: string[];
   routeSubscription?: Subscription;
+  updateBlogPostSubscription?: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private blogPostService: BlogPostService,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -44,7 +47,30 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
     });
   }
 
-  onFormSubmit(): void {}
+  onFormSubmit(): void {
+    // Converting DTO Model for Request Object
+    if (this.model && this.id) {
+      var updateBlogPost: UpdateBlogPost = {
+        author: this.model.author,
+        content: this.model.content,
+        shortDescription: this.model.shortDescription,
+        featuredImageUrl: this.model.featuredImageUrl,
+        isVisible: this.model.isVisible,
+        publishedDate: this.model.publishedDate,
+        title: this.model.title,
+        urlHandle: this.model.urlHandle,
+        categories: this.selectedCategories ?? [],
+      };
+    }
+
+    this.updateBlogPostSubscription = this.blogPostService
+      .updateBlogPost(this.id, updateBlogPost)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/BlogPosts');
+        },
+      });
+  }
 
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
