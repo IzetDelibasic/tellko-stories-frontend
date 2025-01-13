3063f6e402e446482b4487c01d6f543a086a6542
Izet Delibasic <izetdelibasic55@gmail.com>
2025-01-13 01:26:56 +0100

    Implementing auth guard

---
+++ src/app/features/auth/auth.guard.ts
@@ -0,0 +1,45 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from './services/auth.service';
+import { jwtDecode } from 'jwt-decode';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const cookieService = inject(CookieService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getUser();
+
+  // Checking for JWT Token
+  let token = cookieService.get('Authorization');
+
+  if (token) {
+    token = token.replace('Bearer ', '');
+    const decodedToken: any = jwtDecode(token);
+
+    // Checking if token has expired
+    const expirationDate = decodedToken.exp * 1000;
+    const currentTime = new Date().getTime();
+
+    if (expirationDate < currentTime) {
+      authService.logout();
+      return router.createUrlTree(['/login'], {
+        queryParams: { returnUrl: state.url },
+      });
+    } else {
+      // If token valid
+      if (user?.roles.indexOf('Writer')) {
+        return true;
+      } else {
+        alert('Unauthorized');
+        return false;
+      }
+    }
+  } else {
+    // Logout
+    authService.logout();
+    return router.createUrlTree(['/login'], {
+      queryParams: { returnUrl: state.url },
+    });
+  }
+};
